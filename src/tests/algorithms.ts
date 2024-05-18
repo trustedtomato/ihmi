d0876ad816162208b4ee69b450a6f20efb0165d2
Tamas Halasi <trusted.tomato@gmail.com>
2024-05-18 21:31:38 +0200

    add all algorithms to algorithms.ts

--- src/tests/algorithms.ts
+++ src/tests/algorithms.ts
@@ -3,8 +3,10 @@ import { algCotFewshot } from '../algorithms/alg-cot-fewshot.js'
 import { algCotZeroshot } from '../algorithms/alg-cot-zeroshot.js'
 import { algFewshot } from '../algorithms/alg-fewshot.js'
 import { algSplitCotFewshotAlt } from '../algorithms/alg-split-cot-fewshot-alt.js'
+import { algSplitFewshot } from '../algorithms/alg-split-fewshot.js'
 import { algSplitZeroshot } from '../algorithms/alg-split-zeroshot.js'
 import { algTotZeroshot } from '../algorithms/alg-tot-zeroshot.js'
+import { algZeroshotNoAnswerPrefix } from '../algorithms/alg-zeroshot-no-answer-prefix.js'
 import { algZeroshot } from '../algorithms/alg-zeroshot.js'
 
 export const algorithms = [
@@ -13,7 +15,9 @@ export const algorithms = [
   algCotZeroshot,
   algFewshot,
   algSplitCotFewshotAlt,
+  algSplitFewshot,
   algSplitZeroshot,
   algTotZeroshot,
-  algZeroshot
+  algZeroshot,
+  algZeroshotNoAnswerPrefix
 ]

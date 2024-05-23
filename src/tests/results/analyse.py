2f97ca40ed0f1961eabc29d727a7c1b5524abfe8
Tamas Halasi <trusted.tomato@gmail.com>
2024-05-23 18:04:54 +0200

    plot multiple cases in a single figure, make labels consistent

--- src/tests/results/analyse.py
+++ src/tests/results/analyse.py
@@ -16,146 +16,169 @@ def strip_indent(text: str) -> str:
     return '\n'.join(line.strip() for line in text.strip().split('\n'))
 
 # load results
-results_file = sys.argv[1] if len(sys.argv) > 1 else './results.json'
-with open(results_file, 'r') as f:
-    results = json.load(f)
+results_files = sys.argv[1::] if len(sys.argv) > 1 else ['./results.json']
 
-scores = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: {
-    'correctResponses': set(),
-    'correctVotes': 0,
-    'voting': {},
-    'time': 0
-}))))
+def get_acc_scores(results_file):
+    print(f'Processing {results_file}...')
 
-# process results
-for item in results:
-    dataset = item['dataset']
-    algorithm = item['algorithm']
-    model = item['model']
-    test = item['test']
-    score = item['score']
-    result = item['result']
-    time = item['time']
+    with open(results_file, 'r') as f:
+        results = json.load(f)
 
-    test_entry = scores[algorithm][model][dataset][test]
-    test_entry['time'] += time
+    scores = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: {
+        'correctResponses': set(),
+        'correctVotes': 0,
+        'voting': {},
+        'time': 0
+    }))))
 
-    parsed_result, error = try_catch(lambda: json.loads(result))
-    if error:
-        print(strip_indent(f"""
-            CAN'T PARSE RESULT
-            Dataset: {dataset}
-            Algorithm: {algorithm}
-            Model: {model}
-            Test: {test}
-            Result: {result}
-            Time: {time}
-        """) + '\n')
-        continue
+    # process results
+    for item in results:
+        dataset = item['dataset']
+        algorithm = item['algorithm']
+        model = item['model']
+        test = item['test']
+        score = item['score']
+        result = item['result']
+        time = item['time']
 
-    parsed_result = json.dumps(sorted(r['trackingId'] for r in parsed_result))
-    test_entry['voting'][parsed_result] = test_entry['voting'].get(parsed_result, 0) + 1
+        test_entry = scores[algorithm][model][dataset][test]
+        test_entry['time'] += time
 
-    if score == 1:
-        test_entry['correctResponses'].add(parsed_result)
-        test_entry['correctVotes'] += 1
+        parsed_result, error = try_catch(lambda: json.loads(result))
+        if error:
+            print(strip_indent(f"""
+                CAN'T PARSE RESULT
+                Dataset: {dataset}
+                Algorithm: {algorithm}
+                Model: {model}
+                Test: {test}
+                Result: {result}
+                Time: {time}
+            """) + '\n')
+            continue
 
-# aggregate scores (also for self-consistency scores)
-scores_acc = {}
+        parsed_result = json.dumps(sorted(r['trackingId'] for r in parsed_result))
+        test_entry['voting'][parsed_result] = test_entry['voting'].get(parsed_result, 0) + 1
 
-for algorithm, models in scores.items():
-    for model, datasets in models.items():
-        for dataset, tests in datasets.items():
-            for test, details in tests.items():
-                correct_responses = details['correctResponses']
-                voting = details['voting']
-                
-                most_voted = max(voting.items(), key=lambda x: x[1], default=(None, 0))
-                most_voted_result = most_voted[0]
-                is_vote_correct = most_voted_result in correct_responses
+        if score == 1:
+            test_entry['correctResponses'].add(parsed_result)
+            test_entry['correctVotes'] += 1
 
-                if algorithm not in scores_acc:
-                    scores_acc[algorithm] = {}
-                if model not in scores_acc[algorithm]:
-                    scores_acc[algorithm][model] = {
-                        'correct': 0,
-                        'total': 0,
-                        'individualCorrect': 0,
-                        'individualTotal': 0,
-                        'timeTotal': 0
-                    }
+    # aggregate scores (also for self-consistency scores)
+    scores_acc = {}
 
-                model_entry = scores_acc[algorithm][model]
-                model_entry['timeTotal'] += details['time'] / 1000 / 420
-                model_entry['total'] += 1
-                model_entry['individualTotal'] += sum(voting.values())
+    for algorithm, models in scores.items():
+        for model, datasets in models.items():
+            for dataset, tests in datasets.items():
+                for test, details in tests.items():
+                    correct_responses = details['correctResponses']
+                    voting = details['voting']
+                    
+                    most_voted = max(voting.items(), key=lambda x: x[1], default=(None, 0))
+                    most_voted_result = most_voted[0]
+                    is_vote_correct = most_voted_result in correct_responses
 
-                if is_vote_correct:
-                    model_entry['correct'] += 1
-                else:
-                    if (algorithm == 'algSplitCotFewshotAlt' or algorithm == 'algCotFewshotAlt' or algorithm == 'algCotZeroshot') and model == 'llama3':
-                        print(strip_indent(f"""
-                            Dataset: {dataset}
-                            Algorithm: {algorithm}
-                            Model: {model}
-                            Test: {test}
-                            Votes: {json.dumps(voting)}
-                        """) + '\n')
+                    if algorithm not in scores_acc:
+                        scores_acc[algorithm] = {}
+                    if model not in scores_acc[algorithm]:
+                        scores_acc[algorithm][model] = {
+                            'correct': 0,
+                            'total': 0,
+                            'individualCorrect': 0,
+                            'individualTotal': 0,
+                            'timeTotal': 0
+                        }
 
-                model_entry['individualCorrect'] += details['correctVotes']
+                    model_entry = scores_acc[algorithm][model]
+                    model_entry['timeTotal'] += details['time'] / 1000 / 420
+                    model_entry['total'] += 1
+                    model_entry['individualTotal'] += sum(voting.values())
+
+                    if is_vote_correct:
+                        model_entry['correct'] += 1
+                    else:
+                        if (algorithm == 'algSplitCotFewshotAlt' or algorithm == 'algCotFewshotAlt' or algorithm == 'algCotZeroshot') and model == 'llama3':
+                            print(strip_indent(f"""
+                                Dataset: {dataset}
+                                Algorithm: {algorithm}
+                                Model: {model}
+                                Test: {test}
+                                Votes: {json.dumps(voting)}
+                            """) + '\n')
+
+                    model_entry['individualCorrect'] += details['correctVotes']
+    
+    return scores_acc
+
+results = list(map(get_acc_scores, results_files))
 
 # Make plots comparing all algorithms for each model
 models = ['llama3', 'phi3']
 model_names = ['Llama 3', 'Phi-3']
 
+first_scores_acc = results[0]
+times_taken = []
+labels = []
+for algorithm, models in first_scores_acc.items():
+    _model, details = list(models.items())[0]
+    times_taken.append(details['timeTotal'])
+    labels.append(f'{algorithm}')
+
 for (model, model_name) in zip(models, model_names):
 
-  accuracies = []
-  accuracies_individual = []
-  times_taken = []
-  labels = []
-  for algorithm, models in scores_acc.items():
-      for _model, details in models.items():
-        if _model == model:
-          accuracies_individual.append(details['individualCorrect'] / details['individualTotal'])
-          accuracies.append(details['correct'] / details['total'])
-          times_taken.append(details['timeTotal'])
-          labels.append(f'{algorithm}')
-  colors = ['red', 'blue', 'green', 'orange', 'purple', 'yellow', 'pink', 'gray', 'black', 'cyan']
+    # Font size
+    font_size = 12
 
-  # Sort the data according to the x position (time taken)
-  sorted_data = sorted(zip(times_taken, accuracies, accuracies_individual, labels, colors))
+    # colors = plt.cm.tab10.colors
 
-  # Unzip the sorted data
-  times_taken, accuracies, accuracies_individual, labels, colors = zip(*sorted_data)
+    # Set the font to be LaTeX
+    plt.rc('text', usetex=True)
+    plt.rc('font', family='serif', size=font_size)
 
-  # Font size
-  font_size = 12
+    # Create a scatter plot
+    plt.figure(figsize=(10, 6))
 
-  # colors = plt.cm.tab10.colors
+    plt.ylim(0.2, 1)
+    plt.xlim(0, 2)
 
-  # Set the font to be LaTeX
-  plt.rc('text', usetex=True)
-  plt.rc('font', family='serif', size=font_size)
+    # Sort the data according to the x position (time taken)
+    first_sorted_data = sorted(zip(times_taken, labels))
+    label_order = [label for _, label in first_sorted_data]
 
-  # Create a scatter plot
-  plt.figure(figsize=(10, 6))
+    first_run = True
+    colors = ['red', 'blue', 'pink', 'green', 'yellow', '#6f6', 'orange', 'cyan', 'black', 'gray']
+    for scores_acc in results:
+        accuracies = []
+        accuracies_individual = []
+        times_taken = []
+        labels = []
+        for algorithm, models in scores_acc.items():
+            for _model, details in models.items():
+                if _model == model:
+                    accuracies_individual.append(details['individualCorrect'] / details['individualTotal'])
+                    accuracies.append(details['correct'] / details['total'])
+                    times_taken.append(details['timeTotal'])
+                    labels.append(f'{algorithm}')
 
-  plt.ylim(0, 1)
-  plt.xlim(0, 14)
+        # Sort so that the order of the labels is the same as in label_order
+        sorted_indices = [label_order.index(label) for label in labels]
+        sorted_data = sorted(zip(sorted_indices, times_taken, accuracies, accuracies_individual, labels))
+        _, times_taken, accuracies, accuracies_individual, labels = zip(*sorted_data)
+        
+        for i, label in enumerate(labels):
+            plt.scatter(times_taken[i], accuracies[i], marker='+', color=colors[i])
+            plt.scatter(times_taken[i], accuracies_individual[i], marker='x', color=colors[i], label=(label if first_run else None))
 
-  for i, label in enumerate(labels):
-    plt.scatter(times_taken[i], accuracies[i], marker='+', color=colors[i])
-    plt.scatter(times_taken[i], accuracies_individual[i], marker='x', color=colors[i], label=label)
+        first_run = False
 
-  # Add title and labels to the axes
-  plt.title('Accuracy vs Average Run Time (' + model_name + ')')
-  plt.xlabel('Average Run Time (s)')
-  plt.ylabel('Accuracy')
+    # Add title and labels to the axes
+    plt.title('Accuracy vs Average Run Time (' + model_name + ')')
+    plt.xlabel('Average Run Time (s)')
+    plt.ylabel('Accuracy')
 
-  # Add a legend
-  plt.legend(title='Labels', fontsize=font_size, title_fontsize=font_size, loc='lower right')
+    # Add a legend
+    plt.legend(title='Labels', fontsize=font_size, title_fontsize=font_size, loc='lower right')
 
-  # Show the plot
-  plt.grid(True)
-  plt.savefig('plot-' + model + '.pdf', dpi=300, bbox_inches='tight')
+    # Show the plot
+    plt.grid(True)
+    plt.savefig('plot-' + model + '.pdf', dpi=300, bbox_inches='tight')
